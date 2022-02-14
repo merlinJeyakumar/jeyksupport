bf5906a31f953477fafe302d021138767e77dede
Merlin Jeyakumar <s.merlinjeyakumar@gmail.com>
2022-02-14 13:47:22 +0530

    - Utilities updated

---
+++ src/main/java/com/nativedevps/support/inline/Common.kt
@@ -0,0 +1,53 @@
+package com.nativedevps.support.inline
+
+import android.app.Activity
+import android.content.ContentResolver
+import android.content.Context
+import android.net.Uri
+import android.webkit.MimeTypeMap
+import com.google.gson.Gson
+import com.nativedevps.support.utility.device.Common.shareText
+
+import nativedevps.support.R
+import java.util.*
+
+fun Any.toJson(): String? {
+    try {
+        return Gson().toJson(this)
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+    return null
+}
+
+fun Context.getMimeType(uri: Uri): String? {
+    var mimeType: String? = null
+    mimeType = if (uri.scheme == ContentResolver.SCHEME_CONTENT) {
+        contentResolver.getType(uri)
+    } else {
+        val fileExtension: String = MimeTypeMap.getFileExtensionFromUrl(
+            uri
+                .toString()
+        )
+        MimeTypeMap.getSingleton().getMimeTypeFromExtension(
+            fileExtension.toLowerCase(Locale.getDefault())
+        )
+    }
+    return mimeType
+}
+
+fun Context.getSharingText(): String {
+    return "Excited to share it from *${
+        this.resources.getString(
+            R.string.app_name
+        )
+    }* App,\nDownload an app from PlayStore ${getPlayStoreUrl()}"
+}
+
+fun Context.getPlayStoreUrl(): String {
+    return "https://play.google.com/store/apps/details?id=" + applicationContext.packageName
+}
+
+fun Activity.sharePlayStoreUrl() {
+    shareText(description = getSharingText())
+}
--- src/main/java/com/nativedevps/support/utility/device/Common.kt
+++ src/main/java/com/nativedevps/support/utility/device/Common.kt
@@ -1,8 +1,12 @@
 package com.nativedevps.support.utility.device
 
 import android.app.Activity
+import android.app.ActivityManager
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
@@ -10,7 +14,10 @@ import nativedevps.support.R
 import java.io.File
 
 object Common {
-    fun Activity.shareText(description: String) {
+    fun Activity.shareText(
+        description: String,
+        subject: String? =null,
+    ) {
 
         // The value which we will sending through data via
         // other applications is defined
@@ -23,14 +30,14 @@ object Common {
 
         // setting type of data shared as text
         intent.type = "text/plain"
-        intent.putExtra(Intent.EXTRA_SUBJECT, "Subject Here")
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
 
         // Adding the text to share using putExtra
         intent.putExtra(Intent.EXTRA_TEXT, description)
         ContextCompat.startActivity(this, Intent.createChooser(intent, "Share Via"), null)
     }
 
-    fun Context.shareFileText(
+    fun Context.shareFile(
         fileList: List<File>? = null,
         fileMimeType: Array<String> = arrayOf("text/*"),
         emailAddress: String? = null,
@@ -43,8 +50,8 @@ object Common {
             fileList.forEach { file ->
                 uriList.add(
                     FileProvider.getUriForFile(
-                        this@shareFileText,
-                        "${this@shareFileText.packageName}.provider",
+                        this@shareFile,
+                        "${this@shareFile.packageName}.provider",
                         file
                     )
                 )
@@ -70,5 +77,66 @@ object Common {
         startActivity(
             Intent.createChooser(intentShareFile, null).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         )
+    }
+
+    fun Activity.startCall(phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:$phoneNumber"))
+        startActivity(intent)
+    }
+
+    fun getAvailableMemory(): Long {
+        val runTime = Runtime.getRuntime()
+        val usedMemInMB = (runTime.totalMemory() - runTime.freeMemory()) / 1048576L;
+        val maxHeapSizeInMB = runTime.maxMemory() / 1048576L;
+        return maxHeapSizeInMB - usedMemInMB;
+    }
+
+    fun Context.openLinkInternally(text: String) {
+        val defaultBrowser =
+            Intent.makeMainSelectorActivity(Intent.ACTION_MAIN, Intent.CATEGORY_APP_BROWSER)
+        defaultBrowser.data = Uri.parse(text)
+        ContextCompat.startActivity(this, defaultBrowser, null)
+    }
+
+    fun Context.openLinkExternally(text: String) {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(text))
+        ContextCompat.startActivity(this, browserIntent, null)
+    }
+
+    fun Context.pasteClipboardText(): String? {
+        val clipboard: ClipboardManager =
+            getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip: ClipData = clipboard.primaryClip ?: return null
+        val item = clip.getItemAt(0) ?: return null
+        return item.text.toString() ?: return null
+    }
+
+    fun Context.isServiceRunning(serviceClass: Class<*>): Boolean {
+        val manager: ActivityManager =
+            getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (service in manager.getRunningServices(Int.MAX_VALUE)) {
+            if (serviceClass.name == service.service.className) {
+                return true
+            }
+        }
+        return false
+    }
+
+    fun Context.isAppInstalled(packageName: String): Boolean {
+        val pm: PackageManager = packageManager
+        return try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES)
+            true
+        } catch (e: PackageManager.NameNotFoundException) {
+            false
+        }
+    }
+
+    fun openURL(context: Context, url: String) {
+        var url = url
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            url = "http://$url"
+        }
+        context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
     }
 }
