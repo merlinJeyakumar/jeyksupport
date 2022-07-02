eb0254c2563f4002591806c4591af8f676d088cb
jeyakumar.s <jeyakumar.s@optisolbusiness.com>
2022-07-02 21:02:40 +0530

    - Job cancel progress dialog

--- src/main/java/com/nativedevps/support/base_class/BaseViewModel.kt
+++ src/main/java/com/nativedevps/support/base_class/BaseViewModel.kt
@@ -3,9 +3,9 @@ package com.nativedevps.support.base_class
 import android.app.Application
 import android.content.Context
 import android.util.Log
-import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import com.nativedevps.support.model.LoaderProperties
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -16,21 +16,32 @@ import org.jetbrains.anko.toast
 abstract class BaseViewModel constructor(application: Application) :
     AndroidViewModel(application) {
     protected val context: Context get() = getApplication<Application>()
-    val liveDataProgressBar = MutableLiveData<Triple<Boolean, Int, String>>()
+    val liveDataProgressBar = MutableLiveData<LoaderProperties>()
 
     abstract fun onCreate()
 
-    open fun showProgressDialog(message: String = "loading..", progress: Int = -1) {
+    open fun showProgressDialog(
+        message: String = "loading..",
+        progress: Int = -1,
+        cancelable: Boolean = false
+    ) {
         Log.e("JK", "showProgressDialog")
         context.runOnUiThread {
-            liveDataProgressBar.value = Triple(true, progress, message)
+            liveDataProgressBar.value = LoaderProperties(
+                true,
+                message,
+                progress,
+                cancelable
+            )
         }
     }
 
     open fun hideProgressDialog() {
         Log.e("JK", "hideProgressDialog")
         context.runOnUiThread {
-            liveDataProgressBar.value = Triple(false, -1, "")
+            liveDataProgressBar.value = LoaderProperties(
+                false
+            )
         }
     }
 
@@ -48,5 +59,8 @@ abstract class BaseViewModel constructor(application: Application) :
         context.runOnUiThread {
             callback()
         }
+    }
+
+    open fun onProgressDialogCancelled() {
     }
 }
