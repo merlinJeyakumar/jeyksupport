e471960180b1c7a8fa62a3967929eb90f4913413
JeyK <s.merlinjeyakumar@gmail.com>
2022-11-11 20:11:18 +0530

    view

--- src/main/java/com/nativedevps/support/custom_views/dialogs/ListDialog.kt
+++ src/main/java/com/nativedevps/support/custom_views/dialogs/ListDialog.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.appcompat.widget.SearchView
-import androidx.core.view.MenuItemCompat
 import com.nativedevps.support.base_class.dialog.FramedAlertDialog
 import nativedevps.support.R
 import nativedevps.support.databinding.DialogListBinding
