5c90a5c24e0b29958541f2e2e1b1790c60902426
JeyK <s.merlinjeyakumar@gmail.com>
2022-11-12 13:39:02 +0530

    Hilt issue

---
+++ src/main/java/com/nativedevps/support/di/Providers.kt
@@ -0,0 +1,17 @@
+package com.nativedevps.support.di
+
+import com.google.crypto.tink.Aead
+import com.nativedevps.support.utility.encryption.SecurityUtil
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object Providers {
+
+
+
+}
