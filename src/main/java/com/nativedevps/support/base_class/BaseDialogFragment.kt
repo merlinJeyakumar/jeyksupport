e09d456419b08ede4f280aefe83ba4f6aac2df49
JeyK <s.merlinjeyakumar@gmail.com>
2022-12-09 19:49:41 +0530

    - About us dialog update

--- src/main/java/com/nativedevps/support/base_class/BaseDialogFragment.kt
+++ src/main/java/com/nativedevps/support/base_class/BaseDialogFragment.kt
@@ -11,22 +11,31 @@ import androidx.viewbinding.ViewBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import nativedevps.support.R
+import org.jetbrains.anko.layoutInflater
 import org.jetbrains.anko.toast
 
 typealias Inflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
 
 abstract class BaseDialogFragment<VB : ViewBinding, VM : ViewModel>(
-    private val inflate: Inflate<VB>, private val viewModelClass: Class<VM>
+    private val bindingFactory: Inflate<VB>,
+    private val viewModelClass: Class<VM>,
 ) : DialogFragment() {
 
     private var _binding: VB? = null
     val binding get() = _binding!!
+
     protected val viewModel: VM by lazy { ViewModelProvider(this).get(viewModelClass) }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NO_TITLE, theme())
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
     ): View? {
-        _binding = inflate.invoke(inflater, container, false)
+        _binding = bindingFactory.invoke(inflater, container, false)
 
         onInit(savedInstanceState)
         return binding.root
@@ -34,14 +43,16 @@ abstract class BaseDialogFragment<VB : ViewBinding, VM : ViewModel>(
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
     }
 
     open fun onInit(savedInstanceState: Bundle?) {
     }
 
-
     fun toast(string: String) {
         activity?.toast(string)
+    }
+
+    fun theme(): Int {
+        return R.style.TransparentDialogStyle
     }
 }
