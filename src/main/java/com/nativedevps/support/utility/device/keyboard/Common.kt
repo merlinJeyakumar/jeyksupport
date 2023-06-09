a37812f3f0556cf0e5512822643d4acf974a3474
JeyK <s.merlinjeyakumar@gmail.com>
2023-06-09 10:33:18 +0530

    keyboard update

--- src/main/java/com/nativedevps/support/utility/device/keyboard/Common.kt
+++ src/main/java/com/nativedevps/support/utility/device/keyboard/Common.kt
@@ -2,10 +2,14 @@ package com.nativedevps.support.utility.device.keyboard
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Rect
+import android.os.Build
 import android.os.Handler
 import android.view.View
+import android.view.WindowInsets
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import androidx.annotation.RequiresApi
 
 /**
  * Created by Kumar on 08/09/18.
@@ -67,4 +71,17 @@ fun EditText.showKeyboard() {
 
 fun getInputMethodService(context: Context): InputMethodManager {
     return context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+}
+
+
+fun Activity.isKeyboardShowing(): Boolean {
+    val inputMethodManager =
+        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    return inputMethodManager.isActive
+    val rootView = window.decorView
+    val rect = Rect()
+    rootView.getWindowVisibleDisplayFrame(rect)
+    val screenHeight = rootView.height
+    val keyboardHeight = screenHeight - rect.bottom
+    return keyboardHeight > screenHeight * 0.15 // Adjust this threshold as needed
 }
