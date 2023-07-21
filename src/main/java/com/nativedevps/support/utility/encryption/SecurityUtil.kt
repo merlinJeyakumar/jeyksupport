d77b98c0802ef4ab69bb9452318d7d9497453576
JeyK <s.merlinjeyakumar@gmail.com>
2023-07-21 23:24:42 +0530

    boolean fix

--- src/main/java/com/nativedevps/support/utility/encryption/SecurityUtil.kt
+++ src/main/java/com/nativedevps/support/utility/encryption/SecurityUtil.kt
@@ -7,31 +7,62 @@ import java.nio.ByteBuffer
 
 class SecurityUtil(private val aead: Aead) {
 
+    private val TYPE_STRING = 1
+    private val TYPE_BOOLEAN = 2
+    private val TYPE_INT = 3
+    private val TYPE_LONG = 4
+    private val TYPE_DOUBLE = 5
+
     fun encrypt(data: Any): String {
-        val dataByteArray = when (data) {
-            is String -> data.toByteArray(StandardCharsets.UTF_8)
-            is Boolean -> ByteBuffer.allocate(1).put(if (data) 1.toByte() else 0.toByte()).array()
-            is Number -> numberToByteArray(data)
+        val type: Int
+        val dataByteArray: ByteArray = when (data) {
+            is String -> {
+                type = TYPE_STRING
+                data.toByteArray(StandardCharsets.UTF_8)
+            }
+            is Boolean -> {
+                type = TYPE_BOOLEAN
+                ByteBuffer.allocate(1).put(if (data) 1.toByte() else 0.toByte()).array()
+            }
+            is Number -> {
+                type = when (data) {
+                    is Int -> TYPE_INT
+                    is Double -> TYPE_DOUBLE
+                    is Long -> TYPE_LONG
+                    else -> throw IllegalArgumentException("Data type not supported for encryption.")
+                }
+                numberToByteArray(data)
+            }
             else -> throw IllegalArgumentException("Data type not supported for encryption.")
         }
 
-        val encrypted = aead.encrypt(dataByteArray, null)
-        return Base64.encodeToString(encrypted, Base64.DEFAULT).replace("\\r\\n|\\r|\\n".toRegex(), "")
+        val encryptedData = aead.encrypt(dataByteArray, null)
+        val encryptedWithType = ByteBuffer.allocate(encryptedData.size + 1).apply {
+            put(type.toByte())
+            put(encryptedData)
+        }.array()
+
+        return Base64.encodeToString(encryptedWithType, Base64.DEFAULT).replace("\\r\\n|\\r|\\n".toRegex(), "")
     }
 
     fun decrypt(data: String): Any {
-        if (data.isEmpty()) return ""
+        if (data.isEmpty()) {
+            return ""
+        }
 
-        val encryptedBytes = Base64.decode(data.replace("\\r\\n|\\r|\\n".toRegex(), ""), Base64.DEFAULT)
-        val decryptedBytes = aead.decrypt(encryptedBytes, null)
+        val encryptedWithType = Base64.decode(data.replace("\\r\\n|\\r|\\n".toRegex(), ""), Base64.DEFAULT)
+        val type = encryptedWithType[0].toInt()
 
-        return when (byteArrayToType(decryptedBytes)) {
-            is Boolean -> byteArrayToBoolean(decryptedBytes)
-            is Double -> byteArrayToDouble(decryptedBytes)
-            is Long -> byteArrayToLong(decryptedBytes)
-            is Int -> byteArrayToInt(decryptedBytes)
-            is String -> String(decryptedBytes, StandardCharsets.UTF_8)
-            else -> throw IllegalArgumentException("Data type not supported for decryption.")
+        val encryptedData = encryptedWithType.copyOfRange(1, encryptedWithType.size)
+        val decryptedBytes = aead.decrypt(encryptedData, null)
+
+        return when (type) {
+            TYPE_STRING -> String(decryptedBytes, StandardCharsets.UTF_8)
+            TYPE_BOOLEAN -> byteArrayToBoolean(decryptedBytes)
+            TYPE_INT -> byteArrayToInt(decryptedBytes)
+            TYPE_LONG -> byteArrayToLong(decryptedBytes)
+            TYPE_DOUBLE -> byteArrayToDouble(decryptedBytes)
+            else -> throw IllegalArgumentException("Unsupported data type during decryption.")
         }
     }
 
@@ -41,19 +72,6 @@ class SecurityUtil(private val aead: Aead) {
             is Double -> ByteBuffer.allocate(8).putDouble(number).array()
             is Long -> ByteBuffer.allocate(8).putLong(number).array()
             else -> throw IllegalArgumentException("Data type not supported for encryption.")
-        }
-    }
-
-    private fun byteArrayToType(byteArray: ByteArray): Any {
-        return when (byteArray.size) {
-            1 -> byteArrayToBoolean(byteArray)
-            4 -> byteArrayToInt(byteArray)
-            8 -> {
-                val longValue = ByteBuffer.wrap(byteArray).long
-                val doubleValue = longValue.toDouble()
-                if (doubleValue == longValue.toDouble()) longValue else doubleValue
-            }
-            else -> throw IllegalArgumentException("Unsupported byte array size for numeric conversion.")
         }
     }
 
@@ -71,5 +89,4 @@ class SecurityUtil(private val aead: Aead) {
 
     private fun byteArrayToInt(byteArray: ByteArray): Int {
         return ByteBuffer.wrap(byteArray).int
-    }
-}
+    }}
