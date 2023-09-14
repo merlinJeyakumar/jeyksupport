24356b329f033ebbd833cc01424f6b3e4bcccf4f
JeyK <s.merlinjeyakumar@gmail.com>
2023-09-14 14:07:39 +0530

    added: emulate function

--- src/main/java/com/nativedevps/support/utility/usecase/CommonUtils.kt
+++ src/main/java/com/nativedevps/support/utility/usecase/CommonUtils.kt
@@ -1,5 +1,6 @@
 package com.nativedevps.support.utility.usecase
 
+import android.net.Network
 import com.nativedevps.support.coroutines.ErrorApiResult
 import com.nativedevps.support.coroutines.NetworkResult
 import com.nativedevps.support.coroutines.StatusApiResult
@@ -7,6 +8,7 @@ import com.nativedevps.support.coroutines.SuccessApiResult
 import com.nativedevps.support.utility.threading.runOnSameThread
 import kotlinx.coroutines.channels.ProducerScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.first
@@ -22,17 +24,27 @@ suspend fun <T> Flow<NetworkResult<T>>.await(): T {
     }
 }
 
-suspend fun <T,I> Flow<NetworkResult<T>>.await(producerScope: ProducerScope<NetworkResult<I>>?): T {
+suspend fun <T, I> Flow<NetworkResult<T>>.await(producerScope: ProducerScope<NetworkResult<I>>?): T {
     return suspendCancellableCoroutine { cancellableContinuation ->
         val job = runOnSameThread {
             collectLatest { result ->
                 when (result) {
                     is SuccessApiResult -> cancellableContinuation.resume(result.data)
                     is ErrorApiResult -> throw result.throwable ?: Exception(result.message)
-                    is StatusApiResult -> producerScope?.trySend(result as NetworkResult<I>)?: error("handle status api result")
+                    is StatusApiResult -> producerScope?.trySend(result as NetworkResult<I>)
+                        ?: error("handle status api result")
+
                     else -> error("not handled a $result this api")
                 }
             }
         }
+    }
+}
+
+fun <T> emulate(execution: suspend () -> T) = channelFlow<NetworkResult<T>> {
+    try {
+        trySend(SuccessApiResult(execution()))
+    } catch (e: Exception) {
+        trySend(ErrorApiResult(e.message ?: "execute with debug", e))
     }
 }
