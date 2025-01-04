6e8a98c73e2f58dc8dbe647e069e20d7823729ba
JeyK <s.merlinjeyakumar@gmail.com>
2025-01-04 22:28:22 +0400

    updated: changes: theme design update

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,7 +68,8 @@ dependencies {
 }*/
 
 tasks.withType<BundleAar> {
-    val targetDirectory = "D:\\Android\\scrap-note\\modules\\jeyksupport"
+    //val targetDirectory = "D:\\Android\\scrap-note\\modules\\jeyksupport" //scrapnote
+    val targetDirectory = "D:\\Android\\hanuman-chalisa-revamp\\modules\\jeyksupport" //hanumanchalisa
     // Set the archive base name directly as a string
     //archiveFileName.set("nativedevps.aar")
     destinationDirectory.set(file(targetDirectory))
