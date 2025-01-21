18216a5bbfcf7309fbe215270fa57f7a3bd6b3d5
JeyK <s.merlinjeyakumar@gmail.com>
2025-01-21 22:53:03 +0400

    version checking method

--- src/main/java/com/nativedevps/support/utility/text/CommonUtility.kt
+++ src/main/java/com/nativedevps/support/utility/text/CommonUtility.kt
@@ -1,3 +1,18 @@
 package com.nativedevps.support.utility.text
 
 const val ENCODING_UTF_8 = "UTF-8"
+
+fun isVersionCompatible(
+    currentVersion: String,
+    requiredVersion: String
+): Boolean {
+    val currentParts = currentVersion.split(".").map { it.toInt() }
+    val requiredParts = requiredVersion.split(".").map { it.toInt() }
+
+    for (i in 0..2) {
+        if (currentParts[i] > requiredParts[i]) return true
+        if (currentParts[i] < requiredParts[i]) return false
+    }
+
+    return true // If all parts are equal
+}
