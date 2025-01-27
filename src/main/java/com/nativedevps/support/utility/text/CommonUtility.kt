a5d399810a6e5163f66ed213cd82cf583d7e1fe3
JeyK <s.merlinjeyakumar@gmail.com>
2025-01-27 22:57:37 +0400

    Utils update, force update

--- src/main/java/com/nativedevps/support/utility/text/CommonUtility.kt
+++ src/main/java/com/nativedevps/support/utility/text/CommonUtility.kt
@@ -1,5 +1,8 @@
 package com.nativedevps.support.utility.text
 
+import android.content.Context
+import android.content.pm.PackageManager
+
 const val ENCODING_UTF_8 = "UTF-8"
 
 fun isVersionUpdated(requiredVersion: String, currentVersion: String): Boolean {
@@ -11,4 +14,13 @@ fun isVersionUpdated(requiredVersion: String, currentVersion: String): Boolean {
         if (current.getOrElse(i) { 0 } > required[i]) return true
     }
     return true
+}
+
+fun Context.getVersionName(): String? {
+    try {
+        return packageManager.getPackageInfo(packageName, 0).versionName
+    } catch (e: PackageManager.NameNotFoundException) {
+        e.printStackTrace()
+        return null
+    }
 }
